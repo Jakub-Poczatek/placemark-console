2e88d7dd4dfdb6e191983d4fcf41ece70696cb89
Jakub Poczatek <kubapoczatek2001@gmail.com>
2022-09-27 17:02:10 +0100

    Introducing a Controller (MVC)
    Complete.

---
+++ src/main/kotlin/org/setu/placemark/console/controllers/PlacemarkController.kt
@@ -0,0 +1,52 @@
+package org.setu.placemark.console.controllers
+
+import mu.KotlinLogging
+import org.setu.placemark.console.models.PlacemarkMemStore
+import org.setu.placemark.console.models.PlacemarkModel
+import org.setu.placemark.console.views.PlacemarkView
+
+class PlacemarkController {
+    val placemarks = PlacemarkMemStore()
+    val placemarkView = PlacemarkView()
+    val logger = KotlinLogging.logger {}
+
+    init {
+        logger.info("Launching Placemark Console App")
+        println("Placemark Kotlin App Version 1.0")
+    }
+
+    fun menu() : Int {
+        return placemarkView.menu()
+    }
+
+    fun add(){
+        val placemark = PlacemarkModel()
+        if(placemarkView.addPlacementData(placemark))
+            placemarks.create(placemark)
+        else
+            logger.info("Placemark Not Added")
+    }
+
+    fun list() {
+        placemarkView.listAllPlacemarks()
+    }
+
+    fun update() {
+        placemarkView.listAllPlacemarks()
+        var id = placemarkView.getId()
+        var placemark: PlacemarkModel? = placemarks.findOne(id)
+        placemarkView.updatePlacemarkData(placemark)
+    }
+
+    fun search() {
+        val id = placemarkView.getId()
+        val placemark: PlacemarkModel? = placemarks.findOne(id)
+        placemarkView.showPlacemark(placemark)
+    }
+
+    fun dummyData(){
+        placemarks.create(PlacemarkModel(title = "New York New York", description = "So Good They Named It Twice"))
+        placemarks.create(PlacemarkModel(title= "Ring of Kerry", description = "Some place in the Kingdom"))
+        placemarks.create(PlacemarkModel(title = "Waterford City", description = "You get great Blaas Here!!"))
+    }
+}
--- src/main/kotlin/org/setu/placemark/console/main/main.kt
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -1,13 +1,16 @@
 package org.setu.placemark.console.main
 
 import mu.KotlinLogging
+import org.setu.placemark.console.controllers.PlacemarkController
 import org.setu.placemark.console.models.PlacemarkMemStore
 import org.setu.placemark.console.models.PlacemarkModel
 import org.setu.placemark.console.views.PlacemarkView
 
 private val logger = KotlinLogging.logger {}
+
 val placemarks = PlacemarkMemStore()
 val placemarkView = PlacemarkView()
+val controller = PlacemarkController()
 
 fun main(args: Array<String>){
     logger.info {"Launching Placemark Console App"}
@@ -18,44 +21,15 @@ fun main(args: Array<String>){
     do {
         input = placemarkView.menu()
         when (input) {
-            1 -> addPlacemark()
-            2 -> updatePlacemark()
-            3 -> placemarkView.listAllPlacemarks()
-            4 -> searchPlacemark()
-            -99 -> dummyData()
+            1 -> controller.add()
+            2 -> controller.update()
+            3 -> controller.list()
+            4 -> controller.search()
+            -99 -> controller.dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
         println()
     } while (input != -1)
     logger.info {"Shutting Down Placemark Console App"}
-}
-
-fun addPlacemark() {
-    val placemark = PlacemarkModel()
-
-    if(placemarkView.addPlacementData(placemark)){
-        placemarks.create(placemark)
-    } else {
-        logger.info("Placemark Not Added")
-    }
-}
-
-fun updatePlacemark() {
-    placemarkView.listAllPlacemarks()
-    var id = placemarkView.getId()
-    var placemark: PlacemarkModel? = placemarks.findOne(id)
-    placemarkView.updatePlacemarkData(placemark)
-}
-
-fun searchPlacemark(){
-    var id = placemarkView.getId()
-    var placemark: PlacemarkModel? = placemarks.findOne(id)
-    placemarkView.showPlacemark(placemark)
-}
-
-fun dummyData() {
-    placemarks.create(PlacemarkModel(1, "New York New York", "So Good They Named It Twice"))
-    placemarks.create(PlacemarkModel(2, "Ring of Kerry", "Some place in the Kingdom"))
-    placemarks.create(PlacemarkModel(3, "Waterford City", "You get great Blaas Here!!"))
 }
